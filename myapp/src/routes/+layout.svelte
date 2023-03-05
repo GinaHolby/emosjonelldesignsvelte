7bc6516ff5bf2340c21fa5d5e439d62e1f3a02b2
GinaHolby <91539886+GinaHolby@users.noreply.github.com>
2023-03-05 15:44:29 +0100

    first commit

---
+++ myapp/src/routes/+layout.svelte
@@ -0,0 +1,46 @@
+<script>
+    import './app.css';
+</script>
+
+<header>
+    <h1>Oreyly</h1>
+</header>
+
+<div class="headerfill"></div>
+
+<main>
+    <slot></slot>
+
+</main>
+
+
+
+<footer>
+
+    hello :)
+</footer>
+
+<style>
+
+    header{
+        height: 4rem;
+        position: fixed;
+        width: 100%;
+        background-color: aliceblue;
+        padding: 1rem;
+    }
+    .headerfill{
+        height: 4rem;
+    }
+    main{
+        max-width: 900px;
+        margin: auto;
+        border: 2px solid red;
+       
+    }
+
+
+    
+
+
+</style>
---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,75 @@
+<script>
+    // CSS //
+    import './app.css';
+
+    // Animations //
+    import Area from "../animations/Area.svelte";
+    import Clickable from "../animations/Clickable.svelte";
+    import Important from "../animations/Important.svelte";
+    import Noise from "../animations/Noise.svelte";
+    import Thinking from "../animations/Thinking.svelte";
+    import Wheel from "../animations/Wheel.svelte";
+
+</script>
+
+
+    <div class="point one">
+        <div class="text">
+            <h2> 
+                Don´t make me think
+            </h2>
+            <h4>
+                Dont make me think by Steve Krug is about how a web page should be design that is as self explanatory as possible.
+            </h4>
+        </div>
+        <div class="animation"> <Thinking/> </div>
+    </div>
+
+
+
+
+    <div class="point one">
+        <div class="text">
+            <h2> 
+                Don´t make me think
+            </h2>
+            <h4>
+                Dont make me think by Steve Krug is about how a web page should be design that is as self explanatory as possible.
+            </h4>
+        </div>
+        <div class="animation"> <Wheel/> </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+<style>
+   
+    .point{
+        display: flex;
+        gap: 2rem;
+        border: pink solid 2px;
+        width: 100%;
+        min-height: calc(100vh - 4rem);
+        align-items: center;
+    }
+    .text{
+        width: 50%;
+       /*  display: flex;
+        flex-direction: column;
+        justify-content: center;
+        gap: 1rem; */
+    }
+    .animation{
+        width: 50%;
+        
+
+    }
+</style>
